fc58a0cca3c140df4c8ac7bbff5ca37ffe1ddb62
root <root@localhost.localdomain>
2018-11-17 19:22:44 -0500

    Adding OSCE scripts

---
+++ bin_to_python.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+#bin2py created to assist with OSCE payload standardization.
+#Cary Hooper 17NOV18
+
+import binascii
+import sys
+
+if len(sys.argv) != 2:
+	print("[!] Error: requires one argument")
+	print("Usage: ./bin2py /path/to/file.bin")
+	sys.exit()
+
+filename = sys.argv[1]
+with open(filename, 'rb') as file:
+	content = file.read()
+hexfile = binascii.hexlify(content)
+
+init = 0
+print("\n[*] PYTHON")
+for i in range(len(hexfile)):
+	if i % 32 == 0:
+		if init == 0:
+			print("$buf  = \"",end="")
+			init = 1
+		else:
+			print("\"\n$buf += \"",end="")
+	if i % 2 == 0:
+		print("\\x%s%s" % (chr(hexfile[i]),chr(hexfile[i+1])),end="")
+print("\"\n")
+
+print("[*] HEX")
+for i in range(len(hexfile)):	
+	if i % 2 == 0:
+		print("%s%s" % (chr(hexfile[i]),chr(hexfile[i+1])),end="")
+print("\n")
